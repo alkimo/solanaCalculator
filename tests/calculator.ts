d11a57110acb11043e460797e3fb1a7f14eabfe4
alkimo <alkimo@protonmail.com>
2023-04-15 10:33:56 -0300

    Add Demo Project

---
+++ tests/calculator.ts
@@ -0,0 +1,51 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Calculator } from "../target/types/calculator";
+const { SystemProgram } = anchor.web3
+import { expect } from 'chai';
+
+// Prescribed IT blocks
+describe("calculator", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  // Referencing the program - Abstraction that allows 
+  // us to call methods of our SOL program.
+  const program = anchor.workspace.Calculator as Program<Calculator>;
+  const programProvider = program.provider as anchor.AnchorProvider;
+
+  // Generating a keypair for our Calculator account
+  const calculatorPair = anchor.web3.Keypair.generate();
+
+  const text = "Summer School Of Solana";
+
+  // Creating A Test Block
+  it("Creating Calculator Instance", async () => {
+    // Calling create instance - Set our calculator keypar as a signer
+    await program.methods.create(text).accounts(
+      {
+        calculator: calculatorPair.publicKey,
+        user: programProvider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+    }
+  ).signers([calculatorPair]).rpc();
+
+  // We fecth the account and read if the string is actually in the account
+  const account = await program.account.calculator.fetch(calculatorPair.publicKey)
+  expect(account.greeting).to.eql(text)
+  });
+
+  // Another test step - test out addition
+  it('Addition', async () => {
+    await program.methods.add(new anchor.BN(2), new anchor.BN(3))
+    .accounts({ 
+      calculator: calculatorPair.publicKey,
+    })
+    .rpc()
+
+    const account = await program.account.calculator.fetch(calculatorPair.publicKey)
+    expect(account.result).to.deep.equal(new anchor.BN(5))
+  });
+
+  
+});
